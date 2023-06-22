cf5ef21ba6cd9a57f6948723dace9005adeafe09
EmreKnca <114420964+EmreKnca@users.noreply.github.com>
2023-06-23 00:58:20 +0300

    düzgün

--- Assets/Emre/Scripts/PlatformMover.cs
+++ Assets/Emre/Scripts/PlatformMover.cs
@@ -5,31 +5,15 @@ using DG.Tweening;
 
 public class PlatformMover : MonoBehaviour
 {
-    public int PlatformNumber;
+    
 
 
 
-
-    public void PlatformMove1()
+    public void PlatformMove()
     {
-        if (PlatformNumber == 1)
-        {
+       
             this.transform.DOMoveY(transform.position.y - 5, 2f);
-        }
-    }
-    public void PlatformMove2()
-    {
-        if (PlatformNumber == 2)
-        {
-            this.transform.DOMoveY(transform.position.y - 5, 2f);
-        }
-    }
-    public void PlatformMove3()
-    {
-        if (PlatformNumber == 3)
-        {
-            this.transform.DOMoveY(transform.position.y-5,2f);
-        }
         
     }
+   
 }
