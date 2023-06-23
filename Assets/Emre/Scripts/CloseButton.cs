4ba587cd7fdc86ad4cff0387f8196aceb69d3ebf
EmreKnca <114420964+EmreKnca@users.noreply.github.com>
2023-06-23 13:10:25 +0300

    reklam eklenmeden

---
+++ Assets/Emre/Scripts/CloseButton.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloseButton : MonoBehaviour
+{
+    [SerializeField] GameObject InfoPanel;
+    GameObject cursor;
+
+
+
+    private void Start()
+    {
+        cursor =FindObjectOfType<Cursorr>().gameObject;
+        Time.timeScale = 0f;
+        Cursor.lockState = CursorLockMode.None;
+        cursor.SetActive(false);
+    }
+
+    public void ClosePanel()
+    {
+        InfoPanel.SetActive(false);
+        Time.timeScale = 1f;
+        Cursor.lockState = CursorLockMode.Locked;
+        cursor.SetActive(true); 
+    }
+
+}
--- Assets/Emre/Scripts/FallingPlatform.cs
+++ Assets/Emre/Scripts/FallingPlatform.cs
@@ -3,30 +3,49 @@ using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 using UnityEngine.UIElements.Experimental;
+using Etra.StarterAssets.Abilities;
 
 public class FallingPlatform : MonoBehaviour
 {
+    public bool SquidBlock=false;
+    ABILITY_Jump ABILITY_Jump;
+
+    private void Start()
+    {
+        ABILITY_Jump=FindAnyObjectByType<ABILITY_Jump>();
+    }
 
 
-  
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            this.gameObject.transform.DOPunchRotation(Vector3.down, 2, 33,88 ).OnComplete(() =>
+            if (SquidBlock == true)
             {
-            Debug.Log("asdasd1231f");
-            this.transform.DOMoveY(transform.position.y - 15, 3f);
+                ABILITY_Jump.abilityEnabled = false;
+                StartCoroutine(SquidBlockFallingDelay());
             }
+            else
+            {
+                this.gameObject.transform.DOPunchRotation(Vector3.down, 2, 33, 88).OnComplete(() =>
+                {
+                    Debug.Log("asdasd1231f");
+                    this.transform.DOMoveY(transform.position.y - 15, 3f);
+                }
             );
+
+            }
 
         }
     }
    
     
-  
-
-    
+  IEnumerator SquidBlockFallingDelay()
+    {
+        yield return new WaitForSeconds(0.3f);
+        this.transform.DOMoveY(transform.position.y - 15, 5f);
+    }    
        
        
     
