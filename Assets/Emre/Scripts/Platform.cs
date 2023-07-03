717d8ceeb54a49bcc6d4089271564859a4927224
EmreKnca <114420964+EmreKnca@users.noreply.github.com>
2023-07-03 11:39:34 +0300

    22 lwl

--- Assets/Emre/Scripts/Platform.cs
+++ Assets/Emre/Scripts/Platform.cs
@@ -2,12 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using Etra.StarterAssets.Input;
 
 public class Platform : MonoBehaviour
 {
-   
+    GameObject player;
+
+    private void Start()
+    {
+        player = FindObjectOfType<StarterAssetsInputs>().gameObject;
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+       
+            player.transform.parent = transform;
+        
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+            player.transform.parent = null;
+
+    }
 
 
-
-    
 }
