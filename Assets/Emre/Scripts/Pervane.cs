fae6c907545467a61a1f52b21d584f8e2a0ec54f
EmreKnca <114420964+EmreKnca@users.noreply.github.com>
2023-07-10 17:54:45 +0300

    push

---
+++ Assets/Emre/Scripts/Pervane.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Pervane : MonoBehaviour
+{
+    
+    public GameObject PervanePrefab;
+
+
+
+    private void Start()
+    {
+        MoveRight();
+    }
+
+
+    public void MoveRight()
+    {
+    
+            this.gameObject.transform.DORotateQuaternion(Quaternion.Euler(0,100,0),2f).SetEase(Ease.Linear).OnComplete(() =>
+            {
+                ResetRotation();
+            });
+        
+    }
+
+    public void ResetRotation()
+    {
+        this.gameObject.transform.DORotateQuaternion(Quaternion.Euler(0, 230, 0), 2f).SetEase(Ease.Linear).OnComplete(() =>
+        {
+            ResetRotation2();
+        });
+
+    }
+
+    public void ResetRotation2()
+    {
+        this.gameObject.transform.DORotateQuaternion(Quaternion.Euler(0, 360, 0), 2f).SetEase(Ease.Linear).OnComplete(() =>
+        {
+            MoveRight();
+        });
+
+    }
+
+}
